1f27216e92ed6fd059737c6494d93ddc0d091714
orishicha <orishicha@gmail.com>
2021-12-23 14:24:39 +0100

    inclide Heroku

--- index.js
+++ index.js
@@ -333,6 +333,7 @@ app.use((err, req, res, next) => { //error handling
 
 
 // listen for requests
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+    console.log('Listening on Port ' + port);
 });
