78d382eba29c9cb543ecd37a823e4ce52c0f8b4b
orishicha <orishicha@gmail.com>
2022-01-17 14:03:37 +0100

    minor changes

--- index.js
+++ index.js
@@ -37,15 +37,26 @@ app.get('/', passport.authenticate('jwt', { session: false }),
 });
 
 // Get all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }),
-    (req, res) => {
+//temporarily remove the authentication middleware
+// app.get('/movies', passport.authenticate('jwt', { session: false }),
+//     (req, res) => {
+//     Movies.find()
+//         .then((movie) => {
+//             res.json(movie);
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//             res.status(500).send('Error: ' + err);
+//         });
+// });
+app.get('/movies', function (req, res) {
     Movies.find()
-        .then((movie) => {
-            res.json(movie);
+        .then(function (movies) {
+            res.status(201).json(movies);
         })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
+        .catch(function (error) {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
         });
 });
 
